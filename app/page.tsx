6e97f1e8b8cba3f7b21a829af324532b6997d1bf
prabhatsn1 <prabhatsn1@gmail.com>
2025-01-09 11:55:27 +0530

    Home page creation

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/images/story-ai-logo.png";
 
 export default function Home() {
-  return <div>Story teller ai </div>;
+  return (
+    <main className="flex-1 flex flex-col">
+      <section className="flex-1 flex grid-cols-1 lg:grid-cols-2">
+        <div className="bg-purple-500 flex flex-col space-y-5 justify-center items-center order-1 lg:-order-1 pb-10">
+          <Image src={Logo} height={250} alt="logo" />
+          <Button asChild className="px-20 bg-purple-700 p-10 text-xl">
+            <Link href="/stories">Explore Story Library</Link>
+          </Button>
+        </div>
+        {/* Storywriter */}
+      </section>
+    </main>
+  );
 }
