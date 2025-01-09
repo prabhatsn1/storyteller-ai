4bd981456ba707a6ebded71387b6408694d03420
prabhatsn1 <prabhatsn1@gmail.com>
2025-01-09 11:39:25 +0530

    Header Completed

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import { BookOpen, FilePen } from "lucide-react";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header className="realative p-16 text-center">
+      <Link href="/">
+        <h1 className="text-6xl font-black">StoryTeller AI</h1>
+        <div className="flex justify-center whitespace-nowrap space-x-5 text-3xl lg:text-5xl">
+          <h2>Bringing Your Stories</h2>
+          <div className="relative">
+            <div className="absolute bg-purple-500 -left-2 -top-1 -bottom-1 -right-2 md:-left-3 md:-top-0 md:-bottom-0 md:-right-3 -rotate-1" />
+            <p className="relative text-white">To Life!</p>
+          </div>
+        </div>
+      </Link>
+
+      {/* Nav icons */}
+      <div className="absolute -top-5 right-5 flex space-x-2">
+        <Link href="/">
+          <FilePen className="w-8 h-8 lg:w-10 lg:h10 mx-auto text-purple-500 mt-10 border border-purple-500 p-2  rounded-md hover:opacity-50 cursor-pointer" />
+        </Link>
+        <Link href="/">
+          <BookOpen className="w-8 h-8 lg:w-10 lg:h10 mx-auto text-purple-500 mt-10 border border-purple-500 p-2  rounded-md hover:opacity-50 cursor-pointer" />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
